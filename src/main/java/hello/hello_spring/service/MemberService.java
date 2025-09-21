4a85a5a02521f1491e56e8c7935a3411c1939c8f
sujeongmoon <msjk9000@gmail.com>
2025-09-21 19:54:07 +0900

    4-14. 회원 서비스 테스트

--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -5,11 +5,14 @@ import java.util.Optional;
 
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
 
 public class MemberService {
 
-	private final MemberRepository memberRepository = new MemoryMemberRepository();
+	private final MemberRepository memberRepository;
+
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
 
 	/**
 	 * 회원 가입
@@ -23,9 +26,9 @@ public class MemberService {
 
 	private void validateDuplicateMember(Member member) {
 		memberRepository.findByName(member.getName())
-				.ifPresent(m -> {
-					throw new IllegalStateException("이미 존재하는 회원입니다.");
-				});
+			.ifPresent(m -> {
+				throw new IllegalStateException("이미 존재하는 회원입니다.");
+			});
 	}
 
 	/**
