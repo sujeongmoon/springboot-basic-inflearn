fdcc71761fccb0475cd1e2b06e89b4f1e0b1e4b0
sujeongmoon <msjk9000@gmail.com>
2025-09-30 20:21:06 +0900

    6-18. 회원 웹 기능 - 등록

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -2,7 +2,10 @@ package hello.hello_spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 
 @Controller
@@ -13,6 +16,23 @@ public class MemberController {
 	@Autowired
 	public MemberController(MemberService memberService) {
 		this.memberService = memberService;
+	}
+
+	@GetMapping("/members/new")
+	public String createForm() {
+		return "members/createMemberForm.html";
+	}
+
+	@PostMapping("/members/new")
+	public String create(MemberForm form) {
+		Member member = new Member();
+		member.setName(form.getName());
+
+		System.out.println("member = " + member.getName());
+
+		memberService.join(member);
+
+		return "redirect:/";
 	}
 
 }
--- src/main/java/hello/hello_spring/service/MemberService.java
+++ src/main/java/hello/hello_spring/service/MemberService.java
@@ -3,9 +3,6 @@ package hello.hello_spring.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import hello.hello_spring.domain.Member;
 import hello.hello_spring.repository.MemberRepository;
 
